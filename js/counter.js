f995b48d1196de93a0a2507c6aa57114fa9a70aa
Gabriel Rocha <gabriel.rocha20@live.com>
2022-12-30 00:14:53 -0300

    add timer and difficulties

--- js/counter.js
+++ js/counter.js
@@ -1,5 +1,16 @@
 var clicksCounter = 1;
 var heartsCounter = 0;
+var time = document.getElementById("timer");
+var seconds = 15;
+var countDown = setInterval(() => {
+    time.innerHTML = seconds 
+    if(seconds === 0){
+        window.location.replace("victory.html")
+    }
+    seconds--;
+}, 1000);
+
+ 
 
 function verifyCounter(){
     if(clicksCounter > 0){
@@ -13,3 +24,4 @@ function verifyCounter(){
         heart.setAttribute("src","img/coracao_vazio.png")
     }
 }
+
